6355cd9b7c9af0457152220d25481c7bfe9cc6b6
Sean Blake <sean.blake97@outlook.com>
2021-12-29 22:12:57 -0500

    Initial Commit

---
+++ FiveFourLab/FiveFourLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiveFourLabApp.swift
+//  FiveFourLab
+//
+//  Created by SEAN BLAKE on 12/29/21.
+//
+
+import SwiftUI
+
+@main
+struct FiveFourLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
